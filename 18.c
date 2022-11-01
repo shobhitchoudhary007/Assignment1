7a11ce22f608003ca6ef5d7bf894fe98e28d49d0
unknown <shobhitchoudhary76@gmail.com>
2022-11-01 21:43:31 +0530

    18

---
+++ 18.c
@@ -0,0 +1,35 @@
+// Write a C program to calculate profit or loss.
+
+#include <stdio.h>
+int main()
+{
+    int cp,sp, amt; 
+    
+    printf("Enter cost price: ");
+    scanf("%d", &cp);
+    printf("Enter selling price: ");
+    scanf("%d", &sp);
+    
+    if(sp > cp)
+    {
+        amt = sp - cp;
+        printf("Profit = %d", amt);
+    }
+    else if(cp > sp)
+    {
+        amt = cp - sp;
+        printf("Loss = %d", amt);
+    }
+    else
+    {
+        printf("No Profit No Loss.");
+    }
+
+    return 0;
+}
+
+
+// Output
+// Enter cost price: 1000
+// Enter selling price: 1500
+// Profit = 500
